5681aa07feecf64e7a4c15d28a2f492a7dc8d103
Hamadah2O2 <muhammadrozaan@gmail.com>
2024-09-04 10:21:43 +0700

    First Commit

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Permission::create(['name' => 'create-user']);
+        Permission::create(['name' => 'edit-user']);
+        Permission::create(['name' => 'delete-user']);
+        Permission::create(['name' => 'view-user']);
+
+        Role::create(['name' => 'admin']);
+        Role::create(['name' => 'dokter']);
+        Role::create(['name' => 'suster']);
+        Role::create(['name' => 'pasien']);
+    }
+}
